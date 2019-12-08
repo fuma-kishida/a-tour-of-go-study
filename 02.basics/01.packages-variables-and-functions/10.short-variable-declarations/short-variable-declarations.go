3efb891a407af073e875c4757210c55de7ad3a04
kishida-fuma <kishida-fuma@PC-0000003.local>
2019-12-09 01:34:17 +0900

    added short-variable-declarations.go

---
+++ 02.basics/01.packages-variables-and-functions/10.short-variable-declarations/short-variable-declarations.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3  //関数内では、var宣言の代わりに、:= による代入文を使って、暗黙的な型宣言ができる
+	c, python, java := true, false, "no!"  //関数外では、キーワードで始まる宣言が必要 => := による暗黙的な宣言ができない
+
+	fmt.Println(i, j, k, c, python, java)
+}
