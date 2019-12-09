4db5892335fb176fb3cabf26bdc7732f04b50114
kishida-fuma <kishida-fuma@PC-0000003.local>
2019-12-09 16:19:38 +0900

    added interfaces.go

---
+++ 03.methods-and-interfaces/01.methods-and-interfaces/09.interfaces/interfaces.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//interface型は、メソッドのシグニチャ（メソッドを識別するもの）の集まりで定義する
+//メソッドの集まりを実装した値を、interface型の変数に持たせることができる
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f
+	a = &v 
+
+	a = v  // => error : Vertex does not implement Abser (Abs method has pointer receiver)
+
+	fmt.Println(a.Abs())
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+} 
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
