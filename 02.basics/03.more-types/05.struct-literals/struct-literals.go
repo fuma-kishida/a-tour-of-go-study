c662edc0ad5a8974a8a5cc9919ef842b784429ce
kishida-fuma <kishida-fuma@PC-0000003.local>
2019-12-09 09:42:42 +0900

    added struct-literals.go

---
+++ 02.basics/03.more-types/05.struct-literals/struct-literals.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt"
+
+type Vertex struct {
+	X, Y int
+}
+
+var (
+	v1 = Vertex{1, 2}  // => {1, 2}
+	v2 = Vertex{X: 1}  // => {1, 0}
+	v3 = Vertex{}  // => {0, 0}
+	p = &Vertex{1, 2}  // => 新しく割り当てられたstructへのポインタを戻す
+)
+
+func main() {
+	fmt.Println(v1, p, v2, v3)
+}
