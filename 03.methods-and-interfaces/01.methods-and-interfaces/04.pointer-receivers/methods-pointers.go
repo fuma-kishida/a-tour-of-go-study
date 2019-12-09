0e9079f18f423cb797e57b6e71c8c5fdd350692a
kishida-fuma <kishida-fuma@PC-0000003.local>
2019-12-09 15:42:40 +0900

    updated

--- 03.methods-and-interfaces/01.methods-and-interfaces/04.pointer-receivers/methods-pointers.go
+++ 03.methods-and-interfaces/01.methods-and-interfaces/04.pointer-receivers/methods-pointers.go
@@ -14,12 +14,14 @@ func (v Vertex) Abs() float64 {
 }
 
 func (v *Vertex) Scale(f float64) {
+	//ポインタレシーバにすることで、main関数内で v の値を直接書き換えることができる
+	//ポインタレシーバでなく変数レシーバ（=> func (v Vertex) Scale(...)）の場合、ScalaメソッドはVertex変数のコピーを操作するため、main関数内で v の値を書き換えることができない
 	v.X = v.X * f
 	v.Y = v.Y * f
 }
 
 func main() {
-	v := Vertex{3, 4}
-	v.Scale(10)
-	fmt.Println(v.Abs())
+	v := Vertex{3, 4}  //Vertex型のstructが定義される
+	v.Scale(10)  //(変数レシーバのとき)v のコピーに対して、Scalaメソッドを作用させる //(ポインタレシーバのとき)v に対してScalaメソッドを直接作用させる
+	fmt.Println(v.Abs())  // => (変数レシーバのとき) 5 // => (ポインタレシーバのとき) 50
 }
