793c7f84d17553965c5508166fe2585c6ca88acd
kishida-fuma <kishida-fuma@PC-0000003.local>
2019-12-10 16:24:37 +0900

    updated

--- 02.basics/03.more-types/02.structs/stracts.go
+++ 02.basics/03.more-types/02.structs/stracts.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 
-//struct (構造体) は、フィールドの集まり
+//struct (構造体) は、オブジェクト指向言語における class と似たようなもので、関連する変数をひとまとめにする
+//struct (構造体) は、下記のように type と struct を使って定義する
 type Vertex struct {
 	X int
 	Y int
