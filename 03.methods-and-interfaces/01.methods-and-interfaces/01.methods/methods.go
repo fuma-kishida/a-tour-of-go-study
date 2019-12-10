dc4e9263472487de8c9044bd85d0441c4ee4cd67
kishida-fuma <kishida-fuma@PC-0000003.local>
2019-12-10 16:34:58 +0900

    updated

--- 03.methods-and-interfaces/01.methods-and-interfaces/01.methods/methods.go
+++ 03.methods-and-interfaces/01.methods-and-interfaces/01.methods/methods.go
@@ -9,7 +9,9 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {  //func レシーバ （メソッドが定義された型） メソッド名 返り値 {メソッドが行う処理内容}
+//Go には class という概念は無いが、構造体内に method を定義できる
+//Vertex という構造体に対して、Abs というメソッドを定義する
+func (v Vertex) Abs() float64 {  //func レシーバ メソッド名 返り値の型 {メソッドが行う処理内容}
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
